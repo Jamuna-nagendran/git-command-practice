502ed69e625acf03cccfd957e39f461ef44800b8
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2024-02-21 09:10:03 +0530

    Initial main branch commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Calculate c = new Calculate();
+        System.out.println(c.sub(10,5));
+        System.out.println(c.add(5,10));
+    }
+}
