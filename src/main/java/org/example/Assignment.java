dbcb5c4136d1bf41635484bb0a980912b0578c18
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2024-02-21 09:52:02 +0530

    cherry picked from ChildTest to main and hence added Assignment Class

---
+++ src/main/java/org/example/Assignment.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Assignment {
+    public void print(){
+        System.out.println("Hello World!");
+    }
+    public static void add(int a,int b) {
+        System.out.println("The sum of two integers are: " + (a+b));
+    }
+}
