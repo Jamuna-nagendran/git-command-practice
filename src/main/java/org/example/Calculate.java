d2d23cdef32e52e5e5e790ca564bf3762c39ef7f
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2024-02-21 17:01:56 +0530

    Yet to cherry pick on sub()

--- src/main/java/org/example/Calculate.java
+++ src/main/java/org/example/Calculate.java
@@ -2,8 +2,12 @@ package org.example;
 
 public class Calculate {
 
-    public void add(int a,int b){
-        System.out.println("The sum of the two nos are" + (a+b));
+    public void add(int a,int b) {
+        System.out.println("The sum of the two nos are" + (a + b));
 
+    }
+
+    public void sub(int a,int b) {
+        System.out.println("The diff of the two nos are" + (a - b));
     }
 }
