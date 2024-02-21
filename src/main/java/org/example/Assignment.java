25df9bc24dac9b399cac5049747f1c0a3fc0f7be
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2024-02-21 09:52:02 +0530

    updated the existing method and added a new method in Assignment class

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Assignment {
     public void print(){
-        System.out.println("Hello there!");
+        System.out.println("Hello World!");
+    }
+    public static void add(int a,int b) {
+        System.out.println("The sum of two integers are: " + (a+b));
     }
 }
